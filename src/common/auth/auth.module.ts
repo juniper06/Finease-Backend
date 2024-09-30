0f6ac7fbaa0daabf5b6af91771a0edeb0889e249
Juniper Gabriel <junipergabriel62@gmail.com>
2024-09-30 15:25:24 +0800

    Auth Module is updated

--- src/common/auth/auth.module.ts
+++ src/common/auth/auth.module.ts
@@ -1,13 +1,20 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 
 @Module({
   imports: [
-    JwtModule.register({
-      global: true,
-      secret: process.env.SECRET_KEY,
+    ConfigModule.forRoot({
+      isGlobal: true, // Makes the config globally available
+    }),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('SECRET_KEY'),
+      }),
     }),
   ],
   providers: [AuthService],
